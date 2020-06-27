875057cfd8b28497d1719879f2d18541cd01e9f1
George Koniaris <koniaris.giwrgos@gmail.com>
2020-06-27 13:54:34 -0700

    Use env variables instead of strings for confing

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php
 global $settings;
 $settings = [];
-$settings['DB_HOST'] = "localhost";
-$settings['DB_USERNAME'] = "root";
-$settings['DB_PASSWORD'] = "root";
-$settings['DB_NAME'] = "test";
+$settings['DB_HOST'] = getenv("DB_HOST");
+$settings['DB_USERNAME'] = getenv("DB_USERNAME");
+$settings['DB_PASSWORD'] = getenv("DB_PASSWORD");
+$settings['DB_NAME'] = getenv("DB_NAME");
