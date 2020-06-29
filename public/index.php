645761558aa08168ca33729b5b40a3b6bc29cff9
George Koniaris <koniaris.giwrgos@gmail.com>
2020-06-29 03:15:53 -0700

    Make app respect PSR and add autoloader

--- public/index.php
+++ public/index.php
@@ -1,27 +1,12 @@
 <?php
 
-$classesArray = [
-    '../app/Patterns/Singleton.php',
-    '../app/Singletons/Request.php',
-    '../app/Singletons/Response.php',
-    '../app/Singletons/Router.php',
-    '../app/Singletons/Database.php',
-    '../app/Middlewares/BaseMiddleware.php',
-    '../app/Middlewares/Authenticated.php',
-    '../app/Middlewares/GetMethodMiddleware.php',
-    '../app/Middlewares/PostMethodMiddleware.php',
-    '../app/Classes/User.php',
-    '../app/Controllers/BaseController.php',
-    '../app/Controllers/AuthController.php',
-    '../app/Controllers/AuthController.php'
-];
+spl_autoload_register(function ($class) {
+    require_once('../' . str_replace('\\', '/', $class) . '.php');
+});
 
-foreach($classesArray as $item){
-    require_once($item);
-}
+require_once('../App/config.php');
+require_once('../App/middlewares.php');
+require_once('../App/routes.php');
 
-require_once('../app/config.php');
-require_once('../app/middlewares.php');
-require_once('../app/routes.php');
 $request = App\Singletons\Request::getInstance();
 $request->serve();
